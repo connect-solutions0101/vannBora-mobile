89cfe4bc34977a1e5657436aa1fd95d49ae4205e
v-leonel <victor.lima@sptech.school>
2025-03-08 01:17:31 -0300

    feat - Adicionado cores da palheta do projeto

--- app/src/main/java/com/example/mobilevan/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobilevan/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val AmareloTeste = Color(0xFFEEC643)
+
+val AmareloVann = Color(255, 234, 0, 255)
+val CinzaVann = Color(0xFFEEF0F2)
+val AzulVann = Color(0xFF011638)
