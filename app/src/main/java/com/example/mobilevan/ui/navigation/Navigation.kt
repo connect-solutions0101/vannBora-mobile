207c8649a485b1f174fba3865362b88bd6940b9d
v-leonel <victor.lima@sptech.school>
2025-04-13 20:40:17 -0300

    refactor: voltando arquivo Navigation após resolução de conflitos na branch

---
+++ app/src/main/java/com/example/mobilevan/ui/navigation/Navigation.kt
@@ -0,0 +1,19 @@
+package com.example.mobilevan.ui.navigation
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mobilevan.ui.screens.NovoTrajeto
+import com.example.mobilevan.ui.screens.TelaLogin
+import com.example.mobilevan.ui.screens.TrajetosVaziosScreen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.Login.routes) {
+        composable(Routes.Login.routes) { TelaLogin(navController) }
+        composable(Routes.TelaInicial.routes) { TrajetosVaziosScreen(navController) }
+        composable(Routes.NovoTrajeto.routes) { NovoTrajeto(navController) }
+    }
+}
