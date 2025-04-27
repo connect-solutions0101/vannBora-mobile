644ff92c0375167cf07ffd40c04a9770c6008cd7
v-leonel <victor.lima@sptech.school>
2025-04-27 00:12:17 -0300

    refactor: atualiza navigation

--- app/src/main/java/com/example/mobilevan/config/RetrofitConfig.kt
+++ app/src/main/java/com/example/mobilevan/config/RetrofitConfig.kt
@@ -15,9 +15,7 @@ object RetrofitConfig {
 }
 
 object RetrofitConfigHgbr {
-
     private const val BASE_URL = "https://api.hgbrasil.com/"
-
     val api: WeatherApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
--- app/src/main/java/com/example/mobilevan/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/mobilevan/ui/navigation/Navigation.kt
@@ -1,10 +1,13 @@
+package com.example.mobilevan.ui.navigation
+
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mobilevan.ui.navigation.Routes
 import com.example.mobilevan.ui.screens.NovoTrajeto
-import com.example.mobilevan.ui.screens.TrajetoScreen
+import com.example.mobilevan.ui.screens.feature_selecionar_trajeto.TrajetoScreen
 import com.example.mobilevan.ui.screens.feature_clima.ClimaScreen
 import com.example.mobilevan.ui.screens.feature_login.TelaLogin
 
